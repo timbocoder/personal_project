047b646a8c6b6340dedccd42969546e15fd8cb28
Tim Goudie <tigoudie@coca-cola.com>
2016-10-27 21:30:02 -0400

    remove touchstart

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$( '.mobile-menu' ).on( 'click touchstart', function() {
+	$( '.mobile-menu' ).on( 'click', function() {
 		$( '.mobile-menu-options' ).slideToggle();
 
 	})
